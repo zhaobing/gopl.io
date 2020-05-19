a36513a097ded545b7636ec267ed6cf50cd06b14
zhaobing <zhaobing2011@163.com>
2020-05-20 00:03:35 +0800

    add practice

---
+++ prac/chan/du1/du1.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	flag.Parse()
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	fileSizes := make(chan int64)
+
+	go func() {
+		for _, root := range roots {
+			walkDir(root, fileSizes)
+		}
+		close(fileSizes)
+	}()
+
+	var nFiles, nBytes int64
+	for size := range fileSizes {
+		nFiles++
+		nBytes += size
+	}
+
+	printDiskUsage(nFiles, nBytes)
+}
+
+func printDiskUsage(nfiles, nbytes int64) {
+	fmt.Printf("%d files %.1fMB\n", nfiles, float64(nbytes)/1e6)
+}
+
+func walkDir(dir string, fileSizes chan<- int64) {
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subDir := filepath.Join(dir, entry.Name())
+			walkDir(subDir, fileSizes)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+func dirents(dir string) []os.FileInfo {
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du1:%v\n", err)
+		return nil
+	}
+	return entries
+}
