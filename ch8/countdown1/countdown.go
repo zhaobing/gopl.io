fccec52debc3b58f841db1867ad50c81cdb82021
zhaobing <zhaobing2011@163.com>
2020-09-08 18:32:48 +0800

    1. 为公共缓存实现预研

--- ch8/countdown1/countdown.go
+++ ch8/countdown1/countdown.go
@@ -15,6 +15,7 @@ import (
 func main() {
 	fmt.Println("Commencing countdown.")
 	tick := time.Tick(1 * time.Second)
+
 	for countdown := 10; countdown > 0; countdown-- {
 		fmt.Println(countdown)
 		<-tick
